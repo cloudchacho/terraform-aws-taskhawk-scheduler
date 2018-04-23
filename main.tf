0322ec5238b659330e19c668e308665faa54373e
Aniruddha Maru <aniruddhamaru@gmail.com>
2018-04-23 16:50:35 -0700

    HOTFIX: fix math

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_event_rule" "rule" {
-  name                = "taskhawk-${substr(var.name, 0, min(64, length(var.name)))}"
+  name                = "taskhawk-${substr(var.name, 0, min(55, length(var.name)))}"
   description         = "${var.description}"
   schedule_expression = "${var.schedule_expression}"
 }
